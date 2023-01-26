e84b8b006d0c336e12a7b24afaf2ce6bcbb0bebb
John Coffman <john.coffman@slu.edu>
2023-01-26 11:55:01 -0600

    removed some useless things from settings and changed the width and height of the game to feel better

--- Assets/settings.py
+++ Assets/settings.py
@@ -1,26 +1,6 @@
 from random import randint
 
-floor = "X" * 32 
-platform = " " * randint(0, 18) + "X" * 3
-empty = " " * 32
-player = " " * 15 + "P" + " " * 15 
-
-level_map = [ 
-platform,
-empty,
-empty,  
-empty,  
-platform,  
-empty,  
-empty,
-platform,  
-empty,  
-empty, 
-player,  
-floor,
-]
-
 SCROLL_THRESH = 600
 tile_size = 64
-WIDTH = 1280
-HEIGHT = len(level_map) * tile_size
+WIDTH = 400
+HEIGHT = 720
